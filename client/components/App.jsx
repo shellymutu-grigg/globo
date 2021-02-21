115a3591282c27b192c166cc8b3017bb1474780e
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-02-21 14:05:38 +1300

    update structure to be server based

---
+++ client/components/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link, Route } from 'react-router-dom'
+
+import Home from './Home'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        {/* <img src={`/images/${props.match.params.name}.jpg`}></img> */}
+
+        <h1 className="home_header"><img className="header_pic" src="images/Logo.png" alt="Beatles Logo"></img><Link to='/'>    Welcome to BEATLE TUNES!!</Link></h1>
+      </div>
+      {/* This 'main' div is only for styling (so we can use flexbox) */}
+      <div className='main'>
+        <Nav />
+        <Route path='/info/:genre/:name' component={Description} />
+        <Route path='/genre/:genre' component={AllArtists} />
+        <Route exact path='/' component={Home} />
+      </div>
+    </>
+  )
+}
+export default App
