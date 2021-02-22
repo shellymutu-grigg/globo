565c2c0a83d8f869d91de0eef88c0521061289b7
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-02-23 07:21:24 +1300

    atttempt at restructure

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h1 class='heading'>D3 Rotating Globe with Location Marker</h1>
       <h2 class='heading'>Base code by Atanu Mallick</h2>
-      <h3 class='heading'>Currently error with CORS - in process of restructuring</h3>
+      <h3 class='heading'>Error with CORS - in process of restructuring</h3>
     </div>
   )
 }
