b67acac5cbf3088b8f0df053e848aa9631b362b5
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-02-26 20:00:55 +1300

    continuing diagnosis

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h1 class='heading'>D3 Rotating Globe with Location Marker</h1>
       <h2 class='heading'>Base code attributed to Atanu Mallick</h2>
-      <h3 class='heading'>Error with CORS - still in process of restructuring</h3>
+      <h3 class='heading'>Error with CORS - in process of restructuring</h3>
     </div>
   )
 }
