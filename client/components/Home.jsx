bff4e94de33b6acb7f19083cf915cc638a1e9bde
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-02-28 20:40:45 +1300

    continuing diagnosis

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1 class='heading'>D3 Rotating Globe with Location Marker</h1>
-      <h2 class='heading'>Base code attributed to Atanu Mallick</h2>
+      <h2 class='heading'>Based on code attributed to Atanu Mallick</h2>
       <h3 class='heading'>Error with CORS - continuing diagnosis</h3>
     </div>
   )
