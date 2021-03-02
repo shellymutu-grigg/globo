7f2a871209116bf9aff9e138a06000b683fa6b44
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-03-03 12:26:53 +1300

    Updated header

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 class='heading'>D3 Rotating Globe with Location Marker</h1>
+      <h1 class='heading'>D3 Rotating Globe</h1>
       <h2 class='heading'>Based on code attributed to Atanu Mallick</h2>
       <h3 class='heading'>Continuing error with CORS - continuing diagnosis</h3>
     </div>
