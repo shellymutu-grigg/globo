3e42de375a429a354bd6092a5bcee675f42128c3
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-03-04 16:24:08 +1300

    updated header text

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1 class='heading'>D3 Rotating Globe</h1>
-      <h2 class='heading'>Based on code attributed to Atanu Mallick</h2>
+      <h2 class='heading'>Based on code by Atanu Mallick</h2>
       <h3 class='heading'>Continuing error with CORS - continuing diagnosis</h3>
     </div>
   )
