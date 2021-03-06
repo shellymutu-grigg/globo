a511633365ab58759cb28ef8008c1a5648fdb4c6
shellymutu-grigg <shellymutugrigg@gmail.com>
2021-03-06 19:07:00 +1300

    updated headers

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div>
       <h1 class='heading'>Rotating Globe with D3.js</h1>
       <h2 class='heading'>Based on code by Atanu Mallick</h2>
-      <h3 class='heading'>Continuing error with CORS - continuing diagnosis</h3>
+      <h3 class='heading'>Continuing error with CORS - continuing troubleshooting</h3>
     </div>
   )
 }
